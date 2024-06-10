e2abf1224c3ddad65d7f33e0f6136c4a7d80fca5
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2024-06-10 13:56:21 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.2"
+    }  
+}
+
+variable "mycount" {
+  default = 1
+}
+
+resource "random_pet" "pet1" {
+  prefix = timestamp()
+  length = 1
+}
+
+output "pet1" {
+  value = random_pet.pet1.*.id
+}
+
+resource "null_resource" "null1" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "echo ${random_pet.pet1.id}"
+  }
+}
+
+output "null1" {
+  value = null_resource.null1.*.id
+}
+
+resource "null_resource" "null2" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
+output "null2" {
+  value = null_resource.null2.*.id
+}
+
+resource "random_pet" "pet2" {
+  count  = var.mycount
+  prefix = timestamp()
+}
+
+output "pet2" {
+  value = random_pet.pet2.*.id
+}
+
+resource "null_resource" "null3" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
+output "null3" {
+  value = null_resource.null3.*.id
+}
+
+resource "random_pet" "pet3" {
+  count  = var.mycount
+  prefix = timestamp()
+}
+
+output "pet3" {
+  value = random_pet.pet3.*.id
+}
